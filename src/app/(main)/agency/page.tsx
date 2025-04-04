2c5a8357402f0a9b0135647a0b7c4c14595939e8
Ajay Thakare <thakareajay6@gmail.com>
2025-04-04 15:54:01 +0530

    Integrate Prisma with Schema

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,11 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const authUser = await currentUser();
+  if (!authUser) return redirect("/sign-in");
+
   return <div>agency dashboard</div>;
 };
 
