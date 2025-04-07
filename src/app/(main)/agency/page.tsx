2e62d01a86007c278db57f9b31b03f44e3bb52f0
Ajay Thakare <thakareajay6@gmail.com>
2025-04-07 12:26:59 +0530

    feat(server-actions): add getAuthUserDetails, saveActivityLogsNotification, createTeamUser, and verifyAndAcceptInvitation

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,10 +1,16 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const page = async () => {
-  const authUser = await currentUser();
-  if (!authUser) return redirect("/sign-in");
+  const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
+
+  // get user details
+  const user = await getAuthUserDetails();
+
+  // if not user then go to create an agency
 
   return <div>agency dashboard</div>;
 };
